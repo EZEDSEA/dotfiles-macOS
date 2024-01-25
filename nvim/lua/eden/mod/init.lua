5ae0134affc4e726ac2bb81591154118e79be3fd
Hong Zhang <hongoz@amazon.com>
2024-01-25 12:00:25 -0500

    fix: nav keys tmux and nvim

--- nvim/lua/eden/mod/init.lua
+++ nvim/lua/eden/mod/init.lua
@@ -30,7 +30,6 @@ return {
                 up = "<C-k>",
                 right = "<C-l>",
                 last_active = "<C-\\>",
-                -- next = "<C-Space>",
             }
         }
     end
